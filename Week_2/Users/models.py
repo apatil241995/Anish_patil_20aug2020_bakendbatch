3247ceec06535254d8cf3fa44b56aaecc10bf32e
Anish <patilanish39@gmail.com>
2022-02-08 16:21:08 +0530

    models changed

--- Week_2/Users/models.py
+++ Week_2/Users/models.py
@@ -1,18 +1,64 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 
-class UserModel(AbstractBaseUser):
+class UserModelManager(BaseUserManager):
+    def create_user(self, email, date_of_birth, mobile_no, name, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+            mobile_no=mobile_no,
+            name=name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, date_of_birth, mobile_no, name, password=None):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password,
+            date_of_birth=date_of_birth,
+            mobile_no=mobile_no,
+            name=name,
+        )
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+    def create_staff(self, email, date_of_birth, mobile_no, name, password=None):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password,
+            date_of_birth=date_of_birth,
+            mobile_no=mobile_no,
+            name=name,
+        )
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class UserModel(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(
         verbose_name='email address',
         max_length=255,
         unique=True,
     )
     is_active = models.BooleanField(default=True)
-    staff = models.BooleanField(default=False)
-    admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    date_of_birth = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True, blank=False)
     mobile_no = models.IntegerField(unique=True)
     name = models.CharField(max_length=100)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = [mobile_no, name]
+
+    REQUIRED_FIELDS = [mobile_no, name, date_of_birth]
+
+    object = UserModelManager()
--- Week_2/Week_2/settings.py
+++ Week_2/Week_2/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '3%r=nc&2n6+!9)h!0h=qm^^&4tl8mow_l$n@&ggl_m#r*h^8a^'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -74,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Week_2.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -113,7 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -127,8 +123,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'Users.UserModel'
